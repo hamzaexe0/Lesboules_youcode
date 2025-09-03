4204c07ec14f4ab4501e5ae280027da3c00ff2b1
hamzaexe0 <hamzaexe0>
2025-09-03 12:46:36 +0200

    Update challenge 2 and 5

--- Lesboucles_challenge5.c
+++ Lesboucles_challenge5.c
@@ -1,3 +1,13 @@
-//
-// Created by pc on 03/09/2025.
-//
+#include <stdio.h>
+
+int main() {
+    int base, exponent, i;
+    float result = 1;
+    printf("Entrez ton Base apres ton exponent :  \n ");
+    scanf("%d %d", &base, &exponent);
+    for(i = 0; i < exponent; i++) {
+        result *= base;
+        printf("%.0lf \n", result);
+    }
+    return 0;
+}
