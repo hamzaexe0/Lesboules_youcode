d7e154a1e2124761927833b341c4bee4cee184f1
hamzaexe0 <hamzaexe0>
2025-09-03 12:01:03 +0200

    ChallengeN4

---
+++ Lesboucles_challenge4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a,i;
+    int b = 1;
+    printf("Enter your Number: ");
+    scanf("%d", &a);
+    for (i = 1 ; i <= a; ++i) {
+        b = 2 * i - 1;
+        printf("%d ,", b );
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
