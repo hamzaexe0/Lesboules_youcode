7181a75828a8c5ccc9051020cd0ef1200de8f4dd
hamzaexe0 <hamzaexe0>
2025-09-03 10:51:03 +0200

    ChallengeN2

---
+++ Lesboucles_challenge2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    float factorial = 1;
+
+    printf("Entrez un numéro: \n");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Erreur! La factorielle d'un nombre négatif n'existe pas.\n");
+    } else {
+        printf("La factorielle est: ");
+        for (int i = n; i >= 1; --i) {
+            factorial *= i;
+            printf("%d", i);
+
+            if (i > 1) {
+                printf(" * ");
+            }
+        }
+        printf(" = %.1lf\n", factorial);
+    }
+
+    return 0;
+}
